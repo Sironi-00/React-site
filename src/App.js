f9c7bfc702c58110404f78819eb676d64ab702b9
Sironi-00 <00sironi@gmail.com>
2022-08-24 01:27:08 +0100

    progress until issue with category filtration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+var SELECTEDCATEGORY = "*";
+
+export default function App() {
+  const db = [
+    {
+      name: "google",
+      link: "www.google.com",
+      description: "The One Search Engine",
+      category: "search-engine",
+      votes: 0,
+    },
+    {
+      name: "youtube",
+      link: "www.youtube.com",
+      description: "The One Video Hub",
+      category: "video-engine",
+      votes: 0,
+    },
+    {
+      name: "GitHub",
+      link: "www.google.com",
+      description: "The One Search Engine",
+      category: "search-engine",
+      votes: 0,
+    },
+    {
+      name: "FreeCodeCamp",
+      link: "www.youtube.com",
+      description: "The One Video Hub",
+      category: "video-engine",
+      votes: 0,
+    },
+    {
+      name: "SoloLearn",
+      link: "www.google.com",
+      description: "The One Search Engine",
+      category: "search-engine",
+      votes: 0,
+    },
+  ];
+
+  const [dblist, setItem] = useState(db);
+
+  function handleCat(e) {
+    SELECTEDCATEGORY = e.target.value
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th id="cate">
+              <select name="category" id="category" onChange={handleCat}>
+                {/* // display filterd category options */}
+                <option value="*">Category</option>
+                <SelectCate props={dblist} />
+              </select>
+            </th>
+            <th id="name">Name</th>
+            <th id="desc">Description</th>
+            <th id="vote">Votes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Tray props={dblist} />
+        </tbody>
+      </table>
+    </main>
   );
 }
 
-export default App;
+function SelectCate({ props }) {
+  // filters and make category options
+  const histDict = [];
+
+  props.forEach((el) => {
+    if (!(el.category in histDict)) histDict.push({ category: el.category });
+  });
+
+  return histDict.map((el) => {
+    return <option value={el.category}>{el.category}</option>;
+  });
+}
+
+function Tray({ props }) {
+  // Adds and display site to the table
+  return props.map((el) => {
+    if (SELECTEDCATEGORY === el.category || SELECTEDCATEGORY === "*") {
+      return (
+        <tr key={el.name}>
+          <td>{el.category}</td>
+          <td>
+            <a href={el.link}>{el.name}</a>
+          </td>
+          <td>{el.description}</td>
+          <td>{el.votes}</td>
+          <button>UP</button>
+          <button>Down</button>
+        </tr>
+      );
+    } else return <p> The Category doesn't match or the list is empty </p>
+  });
+}
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <nav>
+            <h1>Site-Engine</h1>
+            <ul>
+                <li>Home</li>
+                <li>DEEP</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
